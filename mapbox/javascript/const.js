451340c82ae70e9247198f069c499bec0cf60610
Frederico Belisario <frederico.belisario@coxautoinc.com>
2024-02-07 00:51:59 +0000

    move html variables

---
+++ mapbox/javascript/const.js
@@ -0,0 +1,5 @@
+﻿const config = {
+    login_url: "http://localhost:8081/login",
+    sign_in_url: "http://localhost:8081/signin",
+    cadastro_url: "http://localhost:8081/point"
+}
---
+++ mapbox/javascript/const_docker.js
@@ -0,0 +1,5 @@
+﻿const config = {
+    login_url: "http://point_service:8081/login",
+    sign_in_url: "http://point_service:8081/signin",
+    cadastro_url: "http://point_service:8081/point"
+}
