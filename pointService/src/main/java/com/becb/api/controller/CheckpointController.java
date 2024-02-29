ea76135f4938f642cb0fe5f5a0c9d047780ded21
Frederico Belisario <fbelisario@idirect.net>
2024-02-29 19:40:35 +0000

    Controle para Health

---
+++ pointService/src/main/java/com/becb/api/controller/CheckpointController.java
@@ -0,0 +1,20 @@
+package com.becb.api.controller;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping//( produces = "application/json;charset=UTF-8", consumes = "application/json;charset=UTF-8")
+@JsonAutoDetect(getterVisibility= JsonAutoDetect.Visibility.NONE)
+public class CheckpointController {
+
+
+    @GetMapping(value = "health")
+    @ResponseBody
+    public String health() {
+        return "true";
+    }
+}
