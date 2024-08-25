3b8aecd8c11d4e54522dfaff26d5df00a467965a
Frederico Belisario <Frederico.Belisario@gls-portugal.com>
2024-08-25 10:58:31 +0100

    Add add routes routines

--- processNewPoint/src/main/java/com/becb/processnewpoint/repository/RoteiroRepository.java
+++ processNewPoint/src/main/java/com/becb/processnewpoint/repository/RoteiroRepository.java
@@ -5,15 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-
-import java.util.List;
+import org.springframework.data.repository.query.Param;
 
 public interface RoteiroRepository extends PagingAndSortingRepository<Roteiro,String> {
 
     @Query(value = "SELECT p FROM Roteiro p WHERE p.userOwner.userId = :userId")
     Page<Roteiro> findAllByUserOwner(Pageable pageable, String userId);
 
-    @Query(value = "SELECT p FROM Roteiro p WHERE LOWER(p.city) LIKE LOWER(:city)")
+    @Query(value = "SELECT p FROM Roteiro p WHERE LOWER(p.place) LIKE LOWER(:city)")
     Page<Roteiro> findAllByCity(Pageable pageable, String city);
 
     @Query(value = "SELECT p FROM Roteiro p WHERE LOWER(p.title) LIKE LOWER(:title)")
@@ -23,11 +22,11 @@ public interface RoteiroRepository extends PagingAndSortingRepository<Roteiro,St
             "or (LOWER(p.description) LIKE LOWER(:title))")
     Page<Roteiro> findAllByTitleAndDescription(Pageable pageable, String title);
 
-    @Query(value = "SELECT p FROM Roteiro p WHERE (LOWER(p.city) LIKE LOWER(:city)) " +
+    @Query(value = "SELECT p FROM Roteiro p WHERE (LOWER(p.place) LIKE LOWER(:city)) " +
             "and (LOWER(p.title) LIKE LOWER(:title)) or (LOWER(p.description) LIKE LOWER(:title))")
     Page<Roteiro> findAllByCityAndTitle(Pageable pageable, String city, String title);
 
-    @Query(value = "SELECT p FROM Roteiro p WHERE (LOWER(p.city) LIKE LOWER(:city)) " +
+    @Query(value = "SELECT p FROM Roteiro p WHERE (LOWER(p.place) LIKE LOWER(:city)) " +
             "and (LOWER(p.title) LIKE LOWER(:title)) " +
             "and p.userOwner.userId = :userId")
     Page<Roteiro> findAllByCityAndTitleAndUserOwner(Pageable pageable, String city, String title, String userId);
@@ -36,8 +35,14 @@ public interface RoteiroRepository extends PagingAndSortingRepository<Roteiro,St
             "and p.userOwner.userId = :userId")
     Page<Roteiro> findAllByTitleAndUserOwner(Pageable pageable , String title, String userId);
 
-    @Query(value = "SELECT p FROM Roteiro p WHERE (LOWER(p.city) LIKE LOWER(:city)) " +
+    @Query(value = "SELECT p FROM Roteiro p WHERE (LOWER(p.place) LIKE LOWER(:city)) " +
             "and p.userOwner.userId = :userId")
     Page<Roteiro> findAllByCityAndUserOwner(Pageable pageable , String city, String userId);
 
+    @Query(value = "SELECT p FROM Roteiro p " +
+            "WHERE (:city IS NULL OR LOWER(p.place) LIKE LOWER(:city) ) " +
+            "AND (:userId IS NULL OR p.userOwner.userId = :userId) " +
+            "AND (:title IS NULL OR LOWER(p.title) like LOWER(:title) OR LOWER(p.place) like LOWER(:title))")
+    Page<Roteiro> findAllByRouterFilter(Pageable pageable , @Param("city") String city,
+                                        @Param("userId")String userId,  @Param("title")String title);
 }
